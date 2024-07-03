b109f8916acea37d31a6caf19ebfbe8efc66c333
alphapo040511 <163378726+alphapo040511@users.noreply.github.com>
2024-07-03 16:04:09 +0900

    [level 0] Title: 카운트 다운, Time: 0.24 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[start_num-end_num+1];
+        int t =0;
+        for(int i = start_num; i >= end_num; i--)
+        {
+            answer[t++] = i;
+        }
+        return answer;
+    }
+}
