46ab5bf644a330d806f0eb0a051e4e07443ab316
alphapo040511 <163378726+alphapo040511@users.noreply.github.com>
2024-07-20 15:50:25 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.31 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        if(num_list.Length > 10)
+        {
+            foreach(int i in num_list)
+            {
+                answer += i;
+            }
+        }
+        else
+        {
+            answer = 1;
+            foreach(int i in num_list)
+            {
+                answer *= i;
+            }
+        }
+        
+        return answer;
+    }
+}
