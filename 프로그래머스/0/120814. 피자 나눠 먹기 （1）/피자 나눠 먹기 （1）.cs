d0276ac765df88bac7076be574c57066991ee433
alphapo040511 <163378726+alphapo040511@users.noreply.github.com>
2024-09-03 16:23:44 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.23 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cs
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cs
@@ -2,19 +2,14 @@ using System;
 
 public class Solution {
     public int solution(int n) {
-        if(n <= 7)
-        {
-            return 1;
-        }
-        else
-        {
-            for(int i = 0; ; i++)
+
+            for(int i = 1; ; i++)
             {
                 if(i * 7 >= n)
                 {
                    return i; 
                 }
             }
-        }
+        
     }
 }
