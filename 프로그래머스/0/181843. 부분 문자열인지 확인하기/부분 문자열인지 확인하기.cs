eae5330a5df300515534b818695e60fca57008b1
alphapo040511 <163378726+alphapo040511@users.noreply.github.com>
2024-09-07 22:00:45 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 5.13 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(string my_string, string target) {
+        if(my_string.IndexOf(target) == -1)
+        {
+            return 0;
+        }
+        else
+        {
+return 1;
+}
+    }
+}
