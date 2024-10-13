c5cf3fc58e9986aef40538a6379d036f6609af56
alphapo040511 <163378726+alphapo040511@users.noreply.github.com>
2024-10-13 20:44:18 +0900

    [level 0] Title: 세균 증식, Time: 0.27 ms, Memory: 31.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        answer = (int)(n * Math.Pow(2, t));
+        return answer;
+    }
+}
